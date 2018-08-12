5d9e51f584fdfe7561f8f1aa741f756a03a4bb27
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2018-08-12 21:37:34 +0100

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LambdaWebApp1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //write a LINQ query using Lambda Expressions.
+            //use IEnumerable and store it in "QueryResult"
+            //bind to GridView.
+
+            IEnumerable<Student> QueryResult = Student.GetAllStudents().Where(student => student.Gender == "Male");
+
+            //display the data in a GridView.
+            GridView1.DataSource = QueryResult;
+
+            //invoke the data bind method to the GridView.
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LambdaWebApp1
+{
+    public class Student
+    {
+        //declare the variables in the class.
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public static List<Student> GetAllStudents()
+        {
+            //create a list of students.
+            List<Student> listStudents = new List<Student>();
+
+            //create student objects.
+            Student student1 = new Student
+            {
+                ID = 101,
+                Name = "Mike",
+                Gender = "Male"
+            };
+            //add the student object to the list.
+            listStudents.Add(student1);
+
+            Student student2 = new Student
+            {
+                ID = 102,
+                Name = "Mary",
+                Gender = "Female"
+            };
+            listStudents.Add(student2);
+
+            Student student3 = new Student
+            {
+                ID = 103,
+                Name = "Neil",
+                Gender = "Male"
+            };
+            listStudents.Add(student3);
+
+            Student student4 = new Student
+            {
+                ID = 104,
+                Name = "Steve",
+                Gender = "Male"
+            };
+            listStudents.Add(student4);
+
+            Student student5 = new Student
+            {
+                ID = 105,
+                Name = "Pam",
+                Gender = "Female"
+            };
+            listStudents.Add(student5);
+
+            //here return the list of students back.
+            return listStudents;
+        }
+    }
+}
